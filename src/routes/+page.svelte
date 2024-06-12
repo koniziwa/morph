2e1e38f79df882dd1d850a697920cc79eca501c8
Dmitriy Butskiy <d.butskiy@gmail.com>
2024-06-12 19:00:23 +0300

    VISUAL: Add HTML + a little CSS markup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,72 @@
+<header class="absolute t-0 l-0 w-full px-16 py-8 z-20 flex-row flex justify-between">
+  <div class="flex items-center gap-x-4">
+    <div class="flex w-20 h-20 border-4 border-lime-300 rounded-full items-center justify-center">
+      <div class="overflow-hidden rounded-full w-16 h-16">
+        <img class="w-full h-full" src="./images/killua.jpg" alt="Profile" />
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <p class="text-xl font-medium text-white">koniziwa</p>
+      <p class="text-gray-300">96 lvl</p>
+    </div>
+  </div>
+  <nav class="flex items-center justify-center">
+    <ul class="flex items-center gap-x-8">
+      <li class="text-white font-medium text-lg">My Space</li>
+      <li class="text-gray-400 font-medium text-lg">Music</li>
+      <li class="text-gray-400 font-medium text-lg">Movies</li>
+    </ul>
+  </nav>
+  <div class="flex items-center justify-center">
+    <div class="flex items-begin gap-x-1">
+      <p class="text-white font-medium text-3xl">3:54</p>
+      <p class="text-white text-md">PM</p>
+    </div>
+  </div>
+</header>
+
+<main class="absolute t-0 l-0 w-full h-full">
+  <div class="absolute t-0 l-0 z-10 w-full h-full bg-gradient-to-r from-black via-transparent via-70% to-black to-95% opacity-70"></div>
+  <img class="absolute t-0 l-0 z-0 w-full h-full" src="./images/tlou-bg.jpg" alt="Background">
+  <div class="relative z-10 mt-64 ml-16 flex gap-x-24">
+    <div class="flex flex-col gap-y-4">
+      <p class="text-white font-medium text-2xl">Today</p>
+      <ul class="flex gap-x-2">
+        <li class="flex items-center justify-center rounded-xl w-48 h-48 overflow-hidden">
+          <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+        </li>
+        <li class="flex items-center justify-center rounded-xl w-48 h-48 overflow-hidden">
+          <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+        </li>
+      </ul>
+    </div>
+
+    <div class="flex flex-col gap-y-4">
+      <p class="text-white font-medium text-2xl">Resume</p>
+        <div class="flex items-center justify-center border-4 border-white rounded-xl w-64 h-64">
+          <div class="flex items-center justify-center rounded-xl w-60 h-60 overflow-hidden">
+            <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+          </div>
+        </div>
+    </div>
+
+    <div class="flex flex-col gap-y-4">
+      <p class="text-white font-medium text-2xl">Favorites</p>
+      <ul class="flex gap-x-2">
+        <li class="flex items-center justify-center rounded-xl w-40 h-40 overflow-hidden">
+          <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+        </li>
+        <li class="flex items-center justify-center rounded-xl w-40 h-40 overflow-hidden">
+          <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+        </li>
+        <li class="flex items-center justify-center rounded-xl w-40 h-40 overflow-hidden">
+          <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+        </li>
+        <li class="flex items-center justify-center rounded-xl w-40 h-40 overflow-hidden">
+          <img class="w-full h-full" src="./images/tlou.jpg" alt="The Last Of Us">
+        </li>
+      </ul>
+    </div>
+
+  </div>
+</main>
